80b4bcc10abf69ec33fbd8b6af5ebc8c8f788482
DRAGONINWAVE <yl597591@gmail.com>
2022-10-05 16:29:38 +0800

    rust_first_A_annoying_day

---
+++ funny/target/debug/deps/funny-f0e0e25bf0190c29.d
@@ -0,0 +1,5 @@
+d:\just_for_fun\funny\target\debug\deps\funny-f0e0e25bf0190c29.rmeta: src\main.rs
+
+d:\just_for_fun\funny\target\debug\deps\funny-f0e0e25bf0190c29.d: src\main.rs
+
+src\main.rs:
---
+++ funny/target/debug/deps/funny-f4ddd0961a781e91.d
@@ -0,0 +1,5 @@
+d:\just_for_fun\funny\target\debug\deps\funny-f4ddd0961a781e91.rmeta: src\main.rs
+
+d:\just_for_fun\funny\target\debug\deps\funny-f4ddd0961a781e91.d: src\main.rs
+
+src\main.rs:
