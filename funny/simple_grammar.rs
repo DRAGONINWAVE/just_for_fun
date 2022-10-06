c911bf7711addd8b66cefb06bf3beb611ff16354
DRAGONINWAVE <yl597591@gmail.com>
2022-10-06 21:49:31 +0800

    simple grammar

---
+++ funny/simple_grammar.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x = 1;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is:{}", x);
+    let mut s = "123";
+    // s = s.len();
+    println!("{}", s.len());
+}
