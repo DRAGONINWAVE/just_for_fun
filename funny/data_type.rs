8630e112f47c1c51905248987681d920420ac759
DRAGONINWAVE <yl597591@gmail.com>
2022-10-07 09:45:42 +0800

    data_type

---
+++ funny/data_type.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // float type
+    let x = 2.0;
+    let y: f32 = 3.0;
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    let mut a = [1, 2, 3, 4];
+    let d = [3; 5];
+    a[0] = 4;
+}
