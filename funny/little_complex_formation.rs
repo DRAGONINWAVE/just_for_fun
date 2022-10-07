0d712a6f37e7d1c251404d071263ee6f7ff7ada6
DRAGONINWAVE <yl597591@gmail.com>
2022-10-07 09:59:47 +0800

    funny_language

---
+++ funny/little_complex_formation.rs
@@ -0,0 +1,13 @@
+fn main() {
+    //     let x = 5;
+    //     let y = {
+    //         let x = 3;
+    //         x + 1
+    //     };
+    //     println!("x is {}", x);
+    //     println!("y is {}", y);
+    fn five() -> i32 {
+        5
+    }
+    println!("five() 的值为: {}", five());
+}
