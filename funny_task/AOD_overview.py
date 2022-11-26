9a48e91847e68ed24ffd0cbfd1e6284f51efe85e
DRAGONINWAVE <yl597591@gmail.com>
2022-11-26 20:35:20 +0800

    wuwuwuwu

--- funny_task/AOD_overview.py
+++ funny_task/AOD_overview.py
@@ -1,14 +1,18 @@
 import xarray as xr
 import pandas as pd
-import h5py
+from pandas import HDFStore
+# import h5py
 # import xarray
 
 # data = xr.open_dataset(r"E:\AOD_MYDIS\MYD04_3K.A2003001.0440.061.2018005041406.hdf")
 # print(data['w3'])
 # data = xr.open_dataset(r"E:\AOD_MYDIS\MYD04_3K.A2003001.0440.061.2018005041406.hdf")
 # print(data)
-file = xr.open_dataset(
-    r"E:\AOD_MYDIS\MYD04_3K.A2003001.0440.061.2018005041406.hdf")
+file = HDFStore(
+    r"E:\AOD_MYDIS\MYD04_3K.A2003001.0440.061.2018005041406.h5")
 print(file)
+# file = xr.open_dataset(
+#     r"E:\AOD_MYDIS\MYD04_3K.A2003001.0440.061.2018005041406.hdf")
+# print(file)
 
 # h5py.run_tests()
---
+++ funny_task/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+ataset(
+#     r"E:\AOD_MYDIS\MYD04_3K.A2003001.0440.061.2018005041406.hdf")
+# print(file)
