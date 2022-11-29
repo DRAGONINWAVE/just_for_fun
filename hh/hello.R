aab2519b206b43cdab620f5e212cd8f3db00b811
DRAGONINWAVE <yl597591@gmail.com>
2022-11-29 19:52:44 +0800

    stupid_R

---
+++ hh/hello.R
@@ -0,0 +1,24 @@
+library(openxlsx)
+# z <- c(1:3, NA)
+# z
+# ind <- is.na(z)
+# ind
+# z[is.na(z)] <- 0
+# z
+# # is.nan()
+# # is.finite()
+# paste("My", "Job")
+# labs <- paste("X", 1:6, sep = " ")
+# labs
+# # v <- 10:20
+# v <- c(1, 2, 3, 4)
+# v[-1:-3]
+setwd("D:\\piles\\data (1)\\data\\DATA")
+# x <- read.table("Disease.txt", header = T)
+# y <- read.csv("Growth.csv")
+# all <- merge(x, y, by = "TreeID")
+# all
+products <- read.xlsx("products.xlsx")
+products
+Growth <- read.xlsx("Growth.xlsx")
+Growth
