d42807e69bd151b51507e3b3cfb384e17fd54b42
DRAGONINWAVE <yl597591@gmail.com>
2022-12-06 18:46:40 +0800

    basic_R

--- hh/hello.R
+++ hh/hello.R
@@ -1,4 +1,4 @@
-library(openxlsx)
+# library(openxlsx)
 # z <- c(1:3, NA)
 # z
 # ind <- is.na(z)
@@ -13,12 +13,17 @@ library(openxlsx)
 # # v <- 10:20
 # v <- c(1, 2, 3, 4)
 # v[-1:-3]
-setwd("D:\\piles\\data (1)\\data\\DATA")
+setwd("D:\\piles\\data (1)\\data")
 # x <- read.table("Disease.txt", header = T)
 # y <- read.csv("Growth.csv")
-# all <- merge(x, y, by = "TreeID")
-# all
-products <- read.xlsx("products.xlsx")
-products
-Growth <- read.xlsx("Growth.xlsx")
-Growth
+# all <- merge(x, y)
+# write.table(all, "all.csv")
+# products <- read.xlsx("products.xlsx")
+# products
+# Growth <- read.xlsx("Growth.xlsx")
+# Growth
+typeandday <- read.csv("typeandday.csv")
+typeandday
+typeandday$type <- as.factor(typeandday$type)
+ba.an <- aov(lm(day ~ type, data = typeandday))
+summary(ba.an)
